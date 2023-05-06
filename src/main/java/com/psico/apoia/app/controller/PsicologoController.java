34ca4c598ccb34cd67f14abf01e81ba8f7ddb7d5
SaraDev <sarathaiseg@gmail.com>
2023-05-06 20:37:09 -0300

    PsicologoController

--- src/main/java/com/psico/apoia/app/controller/PsicologoController.java
+++ src/main/java/com/psico/apoia/app/controller/PsicologoController.java
@@ -3,7 +3,7 @@ package com.psico.apoia.app.controller;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class PsicologController {
+public class PsicologoController {
 
 
 }
